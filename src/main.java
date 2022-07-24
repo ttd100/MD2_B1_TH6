7f07b1366eb0ddf201e99d480dff36bf0bbe47e9
Your Name <dat054760@gmail.com>
2022-07-25 00:16:26 +0700

    up

---
+++ src/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("enter a year: ");
+        year = scanner.nextInt();
+        if (year% 4 == 0){
+            if (year% 100 == 0){
+                if (year% 400 == 0) {
+                    System.out.printf("%d is a leap year",year);
+                }else {
+                    System.out.printf("%d is NOT a leap year",year);
+                }
+            }else {
+                System.out.printf("%d is a leap year", year);
+            }
+        }else {
+            System.out.printf("%d is NOT a leap year",year);
+        }
+    }
+}
